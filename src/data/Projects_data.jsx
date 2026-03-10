d30c07fb3968f98daa5dace200a94aef9893dcfe
SYED SIDDIQ <syedsiddiq104@gmail.com>
2026-03-10 13:31:54 +0530

    fix project images path

--- src/data/Projects_data.jsx
+++ src/data/Projects_data.jsx
@@ -1,43 +1,49 @@
+import img1 from "../assets/projectsImages/image.png";
+import img2 from "../assets/projectsImages/APPLE_TN.webp";
+import img3 from "../assets/projectsImages/SODA.png";
+import img4 from "../assets/projectsImages/2025-Formula1-Sauber-C45-006-1440sw.jpg";
+import img5 from "../assets/projectsImages/image copy.png";
+
 export const projectsData = [
   {
     id: 1,
     title: "E - Commerce",
-    image: "/src/assets/projectsImages/image.png",
+    image: img1,
     live: "https://syedsiddiq104.github.io/iphone_clone/",
     github: "#"
   },
   {
     id: 2,
     title: "Iphone Web",
-    image: '/src/assets/projectsImages/APPLE_TN.webp',
+    image: img2,
     live: "https://syedsiddiq104.github.io/iphone_clone/",
     github: "https://github.com/syedsiddiq104/iphone_clone.git"
   },
   {
     id: 3,
     title: "SODA AD - Only Css",
-    image: "/src/assets/projectsImages/SODA.png",
+    image: img3,
     live: "https://syedsiddiq104.github.io/SODA_AD/",
     github: "https://github.com/syedsiddiq104/SODA_AD.git"
   },
   {
     id: 4,
     title: "PORTFOLIO - HTML , CSS",
-    image: "/src/assets/projectsImages/2025-Formula1-Sauber-C45-006-1440sw.jpg",
+    image: img4,
     live: "https://syedsiddiq104.github.io",
     github: "https://github.com/syedsiddiq104/syedsiddiq104.github.io.git"
   },
   {
     id: 5,
-   title: "PORTFOLIO - HTML , CSS , JS",
-    image: "/src/assets/projectsImages/image.png",
+    title: "PORTFOLIO - HTML , CSS , JS",
+    image: img1,
     live: "https://syedsiddiq104.github.io/MY_PORTFOLIO/",
     github: "https://github.com/syedsiddiq104/MY_PORTFOLIO.git"
   },
   {
     id: 6,
     title: "Blink_it - Dynamic",
-    image: "/src/assets/projectsImages/image copy.png",
+    image: img5,
     live: "https://syedsiddiq104.github.io/Blink_It/",
     github: "https://github.com/syedsiddiq104/Blink_It.git"
   }
